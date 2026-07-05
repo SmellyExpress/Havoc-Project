a10f3130af074e23f808edd13b5c5341ed882ba5
SmellyExpress <anthonyleon3420@gmail.com>
2026-07-05 12:59:28 -0700

    Rename tab from 'Visuals' to 'Havoc' and update options

--- UI.lua
+++ UI.lua
@@ -1,8 +1,8 @@
-UI.AddTab("Visuals", function(tab)
-    local Esp = tab:Section("Esp", "Left", {"Targeting", "Silent"})
+UI.AddTab("Havoc", function(tab)
+    local Esp = tab:Section("Esp", "Left", {"Visuals", "World"})
     if Esp.page == 0 then
         Esp:Toggle("esp_enabled", "Enable ESP")
     elseif Esp.page == 1 then
-        Esp:Toggle("silent_enabled", "Enable Silent")
+        Esp:Toggle("world_enabled", "Enable World ESP")
     end
 end)
